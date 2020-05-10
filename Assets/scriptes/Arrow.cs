cb182636fdadda4695042fa15476291df826ff56
Hassan Omar <49106965+Hassan-Omar@users.noreply.github.com>
2020-05-10 17:45:30 +0200

    add rotation to control

---
+++ Assets/scriptes/Arrow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This Class Created by H.Omar to Handle Updating lenght of the dashing arrow 
+/// </summary>
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] GameObject[] arrowPoints; 
+    // static object for external access only 
+    public static Arrow Instance;
+    private void Awake()
+    {
+        if(Instance==null)
+        {
+            Instance = this; 
+        }
+    }
+
+    /// <summary>
+    /// this function to update the arrow length with 
+    /// </summary>
+    /// <param name="value"> the value of increment or decrement of length</param>
+    /// <param name="status"> this value to tell update increament or decreament </param>
+    public void updateLength(float value,int status)
+    {
+        switch(status) {
+            case 1:
+            {       // Increament Operation 
+                    // first point of arrow is fixed 
+                    // I should Move last point and second will be in the middle 
+                    arrowPoints[2].transform.localPosition += new Vector3(0, value, 0);
+                    arrowPoints[1].transform.localPosition += new Vector3(0, value, 0);
+                    break;
+            }
+            case -1:
+            {       // Decreament Operation 
+                    // first point of arrow is fixed 
+                    // I should Move last point and second will be in the middle 
+                    arrowPoints[2].transform.localPosition -= new Vector3(0, value, 0);
+                    arrowPoints[1].transform.localPosition -= new Vector3(0, value, 0);
+                    break;
+            }
+        }
+    }
+    /// <summary>
+    /// function to reset the arrow lenght 
+    /// </summary>
+    public void resetToStartLenght()
+    {
+        arrowPoints[0].transform.localPosition = new Vector3(0, 0.09f, 0);
+        arrowPoints[1].transform.localPosition = new Vector3(0, 0.15f, 0);
+        arrowPoints[2].transform.localPosition = new Vector3(0, 0.21f, 0);
+    }
+
+}
--- Assets/scriptes/Player.cs
+++ Assets/scriptes/Player.cs
@@ -8,8 +8,14 @@ public class Player : MonoBehaviour
 {
     // to keep first touch position relative to screen coordinates 
     private Vector2 startTouchPos;
+    // length of arrow before starting controlling  
+    private float startArrowLength;
+    // vars to contorol on Senstivity 
+    public float rotationSenstivity = 0.01f;
+    public float changingLengthSenstivity = 0.01f;
     // reference to arrow to activation or deactivation 
     [SerializeField] GameObject arrow; 
+  
     private void Update()
     {
         // prevent calling if we have no touching 
@@ -29,20 +35,56 @@ public class Player : MonoBehaviour
                     arrow.SetActive(true);
                     // apply slow motion effect 
                     Time.timeScale = 0.5f;
-                    // rotate the player with the moving 
-                    float rotationValue = -(touch.position.x - (Screen.width / 2) )* 0.01f; 
-                    transform.Rotate(new Vector3(0, 0, rotationValue));
+                    // calculate values for rotation and new length to update arrow 
+                    float rotationValue = (touch.position.x - (Screen.width / 2) )* rotationSenstivity;
+                    float lengthValue = Vector2.Distance(startTouchPos, touch.position);
 
-                    // rotate the arrow 
+                    // rotate the arrow opposite to current touched point 
+                    Vector3 diff = Camera.main.ScreenToWorldPoint(touch.position) - transform.position;
+                    diff.Normalize();
+                    float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+                    arrow.transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
+
+
+                    /*// update arrow's length   
+                    if(lengthValue> startArrowLength)
+                    {
+                        // increase Arrow Length 
+                        Arrow.Instance.updateLength(lengthValue * changingLengthSenstivity, 1);
+                    }
+                    else
+                    {
+                        // decrease Arrow Length 
+                        Arrow.Instance.updateLength(lengthValue * changingLengthSenstivity, -1);
+                    }*/
                     break;
-
+                    
                 case TouchPhase.Ended:
                     // end slow motion effect 
                     Time.timeScale = 1;
                     arrow.SetActive(false);
+                    // reset arrow's lengh 
+                    Arrow.Instance.resetToStartLenght();
+                    arrow.transform.localRotation = Quaternion.identity;
                     // here we will project the player 
                     break;
             }
         }
+    }
+
+    /// <summary>
+    /// function to check if the user finger up or down relative to the player 
+    /// </summary>
+    /// <param name="point">touched screen point </param>
+    /// <returns> 1 if up , -1 if down </returns>
+    private int getDirection(Vector2 point)
+    {
+        Vector3 p = Camera.main.ScreenToWorldPoint(point);
+        if (p.y > transform.position.y)
+        {
+            return 1;
+        }
+        else
+            return -1;
     }
 }
