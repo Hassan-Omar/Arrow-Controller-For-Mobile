cd5a7e04c2549f7f9147fe442c59c3c4fc7dac55
Hassan Omar <49106965+Hassan-Omar@users.noreply.github.com>
2020-05-12 12:37:49 +0200

    add attraction effect in Game Play

--- Assets/scriptes/Atrractor.cs
+++ Assets/scriptes/Atrractor.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Atrractor : MonoBehaviour
 {
+    private float attractionForce = 50f;
+    private GameObject player; 
+    [SerializeField]private bool isAttractionEnabled = false; 
+
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
        if (collision.gameObject.tag == "Player")
@@ -13,6 +22,27 @@ public class Atrractor : MonoBehaviour
             rb.gravityScale = 0;
             rb.velocity = Vector2.zero;
             rb.angularVelocity = 0;
+        }
+    }
+    /// <summary>
+    /// function to simulate attraction on player 
+    /// </summary>
+    private void attractPlayer()
+    {
+        var rb = player.GetComponent<Rigidbody2D>(); 
+        rb.gravityScale = 0;
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0;
+        Vector3 forceDirection = transform.position - player.transform.position;
+        rb.AddForce(attractionForce * forceDirection.normalized);
+        Debug.Log("Called With "+ attractionForce);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(this.isAttractionEnabled)
+        {
+            attractPlayer();
         }
     }
 }
